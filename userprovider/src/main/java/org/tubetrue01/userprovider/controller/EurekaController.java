44d9454b19d5206d74534f3a70a0719168f721e8
Tubetrue01 <tubetrue01@gmail.com>
2020-02-11 19:18:32 +0800

    Add lifecycle of pod supports that cancel eureka client registry when pod has been killed;

---
+++ userprovider/src/main/java/org/tubetrue01/userprovider/controller/EurekaController.java
@@ -0,0 +1,26 @@
+package org.tubetrue01.userprovider.controller;
+
+import com.netflix.discovery.DiscoveryManager;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User : Pengfei Zhang
+ * Mail : Tubetrue01@gmail.com
+ * Date : 2020/2/11
+ * Time : 4:18 下午
+ * Description :
+ */
+@Log4j2
+@RestController
+@RequestMapping("/eureka")
+public class EurekaController {
+    @GetMapping(value = "/offline")
+    public void offLine() {
+        log.warn("-==注销服务:[{}]==-", System.getenv("POD_NAME"));
+        DiscoveryManager.getInstance().shutdownComponent();
+    }
+}
