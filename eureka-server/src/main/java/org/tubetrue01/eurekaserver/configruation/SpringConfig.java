c45743a9e7da1f0c99b92fb3da934d41db66150f
Tubetrue01 <tubetrue01@gmail.com>
2020-05-06 10:57:09 +0800

    Update eureka-server module to support prometheus endpoint collected; Other modules will be updated in future;

---
+++ eureka-server/src/main/java/org/tubetrue01/eurekaserver/configruation/SpringConfig.java
@@ -0,0 +1,25 @@
+package org.tubetrue01.eurekaserver.configruation;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User : Pengfei Zhang
+ * Mail : Tubetrue01@gmail.com
+ * Date : 2020/5/5
+ * Time : 3:12 下午
+ * Description :
+ */
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+        return registry -> registry.config().commonTags("application", applicationName);
+    }
+
+}
